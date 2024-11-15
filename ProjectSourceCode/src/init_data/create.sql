6df39300ce2664c31a0fd95d73b925b59b7566f4
Matthew Lieng <mali7617@colorado.edu>
2024-11-15 12:19:30 -0700

    created moods, locations, moods_to_locations, reviews tables

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -5,3 +5,27 @@ CREATE TABLE USERS (
     user_type VARCHAR(25)
 );
 
+create table moods(
+    mood_id serial primary key,
+    mood_name varchar(25)
+);
+
+create table locations(
+    location_id serial primary key,
+    location_name varchar(50),
+);
+
+create table moods_to_locations(
+    mood_id int not null references moods(mood_id),
+    location_id int not null references locations(location_id)
+);
+
+create table reviews{
+    review_id serial primary key,
+    user_id int references users(user_id),
+    location_id int references locations(location_id),
+    mood_id int references moods(mood_id),
+    rating int,
+    review varchar(250)
+}
+
