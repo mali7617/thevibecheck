870458b0eb75accfca8c52ac88376c9808035ffb
Austin Hutchen <austinhutchen@Austins-MacBook-Air.local>
2024-12-02 19:33:54 -0700

    pwd

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,31 +1,31 @@
-create table users (
-    user_id SERIAL primary key,
-    username VARCHAR(50) not null,
-    pwd VARCHAR(60) not null,
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    pwd VARCHAR(60) NOT NULL,
     user_type VARCHAR(25)
 );
 
-create table moods(
-    mood_id serial primary key,
-    mood_name varchar(25)
+CREATE TABLE IF NOT EXISTS moods (
+    mood_id SERIAL PRIMARY KEY,
+    mood_name VARCHAR(25)
 );
 
-create table locations(
-    location_id serial primary key,
-    location_name varchar(50)
+CREATE TABLE IF NOT EXISTS locations (
+    location_id SERIAL PRIMARY KEY,
+    location_name VARCHAR(50)
 );
 
-create table moods_to_locations(
-    mood_id int not null references moods(mood_id),
-    location_id int not null references locations(location_id)
+CREATE TABLE IF NOT EXISTS moods_to_locations (
+    mood_id INT NOT NULL REFERENCES moods(mood_id),
+    location_id INT NOT NULL REFERENCES locations(location_id),
+    PRIMARY KEY (mood_id, location_id)
 );
 
-create table reviews(
-    review_id serial primary key,
-    user_id int references users(user_id),
-    location_id int references locations(location_id),
-    mood_id int references moods(mood_id),
-    rating int,
-    review varchar(250)
+CREATE TABLE IF NOT EXISTS reviews (
+    review_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    location_id INT REFERENCES locations(location_id),
+    mood_id INT REFERENCES moods(mood_id),
+    rating INT,
+    review VARCHAR(250)
 );
-
