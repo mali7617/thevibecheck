6db0d3cddc2dacf52084ee445ed584752a3b0ee5
Austin Hutchen <austinhutchen@Austins-MacBook-Air.local>
2024-12-02 20:37:41 -0700

    env

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,27 +1,27 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     pwd VARCHAR(60) NOT NULL,
     user_type VARCHAR(25)
 );
 
-CREATE TABLE IF NOT EXISTS moods (
+CREATE TABLE moods (
     mood_id SERIAL PRIMARY KEY,
     mood_name VARCHAR(25)
 );
 
-CREATE TABLE IF NOT EXISTS locations (
+CREATE TABLE  locations (
     location_id SERIAL PRIMARY KEY,
     location_name VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS moods_to_locations (
+CREATE TABLE  moods_to_locations (
     mood_id INT NOT NULL REFERENCES moods(mood_id),
     location_id INT NOT NULL REFERENCES locations(location_id),
     PRIMARY KEY (mood_id, location_id)
 );
 
-CREATE TABLE IF NOT EXISTS reviews (
+CREATE TABLE reviews (
     review_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id),
     location_id INT REFERENCES locations(location_id),
